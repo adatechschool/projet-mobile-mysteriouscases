aee12ccfe90dafd5aba0e4c05591f157873c565d
Alan <alan0613@live.fr>
2023-09-01 13:24:06 +0200

    avancement HomePage

--- MysteryQuest/components/Navbar.js
+++ MysteryQuest/components/Navbar.js
@@ -1,40 +1,36 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
 import { Link } from "react-router-native";
-import Svg, { G, Path } from 'react-native-svg'; 
+import Svg, { G, Path } from "react-native-svg";
 
 const Navbar = () => {
-    return (
-        <View style={styles.container}>
-            <Link to="/">
-                <Text style={styles.title}>Mystery Quest</Text>
-            </Link>
-            <Link to="/Menu">
-                <Svg 
-                width={24} 
-                height={24} 
-                viewBox="0 0 448 512"
-                >
-                    <G fill="#eab308">
-                        <Path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
-                    </G>
-                </Svg>
-            </Link>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Link to="/">
+        <Text style={styles.title}>Mystery Quest</Text>
+      </Link>
+      <Link to="/Menu">
+        <Svg width={24} height={24} viewBox="0 0 448 512">
+          <G fill="#eab308">
+            <Path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+          </G>
+        </Svg>
+      </Link>
+    </View>
+  );
 };
 const styles = StyleSheet.create({
-    title: {
-      color: "#EAB308",
-      fontFamily: "Mystery",
-      fontSize: 16,
-      textAlign: "center",
-    },
-    container: {
-        display:"flex",
-        flexDirection:"row",
-        justifyContent:"space-between",
-        paddingHorizontal:20,
-    }
-  });
+  title: {
+    color: "#EAB308",
+    fontFamily: "Mystery",
+    fontSize: 16,
+    textAlign: "center",
+  },
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+  },
+});
 export default Navbar;
--- MysteryQuest/pages/HomePage.js
+++ MysteryQuest/pages/HomePage.js
@@ -1,44 +1,70 @@
+import React from "react";
 import {
   View,
   Text,
   FlatList,
   SafeAreaView,
-  Item,
-  SectionList,
   StyleSheet,
-  Button,
+  TouchableOpacity,
 } from "react-native";
 import Navbar from "../components/Navbar";
+import Svg, { G, Path } from "react-native-svg";
 
 const HomePage = () => {
+  const quests = [
+    {
+      title: "La Malette à Mamie",
+      desc: "Explorez le quartier, résolvez des énigmes et découvrez les souvenirs cachés pour retrouver la malette perdue de Mamie.",
+    },
+    {
+      title: "Où est Camille ?",
+      desc: "Camille, la petite fille que tu devais garder a disparu. Retrouve-la rapidement avant que ses mamans rentrent du travail !",
+    },
+    {
+      title: "Les Ailes d'Élara",
+      desc: "Découvrez les mystères de Paris en aidant la fée Élara à retrouver des fragments magiques et à sauver l'équilibre entre les mondes.",
+    },
+  ];
+
   return (
-    <SafeAreaView>
-      <Navbar/>
-      <Text style={styles.title}>Quêtes disponibles : </Text>
+    <SafeAreaView style={styles.container}>
+      <Navbar />
+      <Text style={styles.title}>Quêtes disponibles :</Text>
       <FlatList
-        data={[
-          {
-            key: "La Malette à Mamie",
-            desc: "Explorez le quartier, résolvez des énigmes et découvrez les souvenirs cachés pour retrouver la malette perdue de Mamie.",
-          },
-          {
-            key: "Où est Camille ?",
-            desc: "Camille, la petite fille que tu devais garder a disparu. Retrouve-la rapidement avant que ses mamans rentrent du travail !",
-          },
-          {
-            key: "Les Ailes d'Élara",
-            desc: "Découvrez les mystères de Paris en aidant la fée Élara à retrouver des fragments magiques et à sauver l'équilibre entre les mondes.",
-          },
-        ]}
-        renderItem={({ item }) => (
-          <View>
-            <Text style={styles.item}>{item.key}</Text>
-            <Text>{item.desc}</Text>
-            <Button title="-->"></Button>
+        data={quests}
+        renderItem={({ item, index }) => (
+          <View key={index} style={styles.questContainer}>
+            <View style={styles.blurryBackground}>
+              <Text style={styles.itemTitle}>{item.title}</Text>
+              <Text style={styles.desc}>{item.desc}</Text>
+              <TouchableOpacity
+                onPress={() =>
+                  console.log("je dois rediriger vers la quete ", index)
+                }
+                style={styles.buttonContainer}
+              >
+                <Svg
+                  width="77"
+                  height="12"
+                  viewBox="0 0 77 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <G>
+                    <Path
+                      d="M0.666667 6C0.666667 8.94552 3.05448 11.3333 6 11.3333C8.94552 11.3333 11.3333 8.94552 11.3333 6C11.3333 3.05448 8.94552 0.666667 6 0.666667C3.05448 0.666667 0.666667 3.05448 0.666667 6ZM77 6L67 0.226497V11.7735L77 6ZM6 7H68V5H6V7Z"
+                      fill="#EAB308"
+                      fillOpacity="0.8" // Utilisez fillOpacity au lieu de fill-opacity
+                    />
+                  </G>
+                </Svg>
+              </TouchableOpacity>
+            </View>
           </View>
         )}
-        style={styles.container}
-      ></FlatList>
+        style={styles.list}
+        keyExtractor={(item) => item.title}
+      />
     </SafeAreaView>
   );
 };
@@ -46,16 +72,46 @@ const HomePage = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 22,
-    textAlign: "center",
-  },
-  item: {
+    margin: 13,
     padding: 10,
-    fontSize: 18,
-    height: 44,
-    textAlign: "center",
   },
-  title: { fontSize: 35, textAlign: "center", paddingTop: 50 },
+  list: {
+    flex: 1,
+    marginTop: 10,
+  },
+  questContainer: {
+    marginBottom: 20, // Ajoutez un espacement entre chaque quête
+  },
+  blurryBackground: {
+    backgroundColor: "rgba(128, 128, 128, 0.3)", // Opacité appliquée ici
+    borderRadius: 25, // Appliquez la bordure au fond flou
+    padding: 10, // Ajoutez un espacement autour du fond flou
+  },
+  itemTitle: {
+    fontSize: 29,
+    textAlign: "center",
+    fontFamily: "Mystery",
+    color: "#EAB308",
+  },
+  desc: {
+    fontFamily: "Baskerville",
+    textAlign: "center",
+    fontSize: 20,
+    color: "#EAB308",
+  },
+  title: {
+    fontSize: 35,
+    textAlign: "center",
+    paddingTop: 50,
+    fontFamily: "Mystery",
+    color: "#EAB308",
+  },
+  buttonContainer: {
+    alignItems: "flex-end", // Centrer horizontalement
+    marginTop: 15,
+    marginBottom: 5,
+    paddingRight: 20,
+  },
 });
 
 export default HomePage;
