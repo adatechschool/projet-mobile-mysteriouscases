c240b0107fb897122a454346c96e3ee96205c3f2
Camille Hebert <camille.hebert22@gmail.com>
2023-09-05 18:36:22 +0200

    connexion inscription overlay

--- MysteryQuest/components/Inscription.js
+++ MysteryQuest/components/Inscription.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { View, StyleSheet, Text, TouchableOpacity, TextInput } from 'react-native';
-import Navbar from './Navbar';
 import Svg, { G, Path, Rect } from "react-native-svg";
 import { useNavigate } from "react-router-native";
 
-const Inscription = () => {
+const Inscription = ({onClose}) => {
 
     const navigate = useNavigate();
 
     return (
         <View>
-            <Navbar/>
             <View style={styles.container}>
-                <TouchableOpacity style={styles.cross}>
-                    <Svg  width="35" height="25" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <TouchableOpacity style={styles.cross} onPress={onClose}>
+                    <Svg  width="20" height="20" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <Path
                             d="M2.275 22.75L0 20.475L9.1 11.375L0 2.275L2.275 0L11.375 9.1L20.475 0L22.75 2.275L13.65 11.375L22.75 20.475L20.475 22.75L11.375 13.65L2.275 22.75Z"
                             fill="#EAB308"
@@ -63,7 +61,7 @@ const Inscription = () => {
                     // onChangeText={handlePasswordChange}
                 />
                 <TouchableOpacity style={styles.button} onPress={() => {navigate("/Connexion")}}>
-                    <Text>Inscription</Text>
+                    <Text>Valider</Text>
                 </TouchableOpacity>
 
             </View>
@@ -74,16 +72,16 @@ const Inscription = () => {
 const styles = StyleSheet.create({
     button: {
       alignItems: 'center',
-      paddingVertical: 12,
-      marginHorizontal: 100,
+      padding: 12,
+      marginHorizontal: 60,
       borderRadius: 15,
       backgroundColor: '#EAB308',
     },
     container:{
-        backgroundColor: "rgba(0, 0, 0, 0.3)", // Opacité appliquée ici
+        backgroundColor: "#1E1B4B", // Opacité appliquée ici
         borderRadius: 25, // Appliquez la bordure au fond flou
-        padding: 10, // Ajoutez un espacement autour du fond flou
-        margin: 20,
+        padding: 20, // Ajoutez un espacement autour du fond flou
+        // margin: 10,
         borderColor: "#eab308",
         borderWidth:1,
         flexDirection:"column"
--- MysteryQuest/pages/LandingPage.js
+++ MysteryQuest/pages/LandingPage.js
@@ -1,24 +1,61 @@
-import { View, Text, Button, StyleSheet, Pressable, TouchableOpacity } from "react-native";
+import { View, Text, Button, StyleSheet, Pressable, TouchableOpacity, Modal } from "react-native";
 import { useNavigate } from "react-router-native";
 import Svg, { G, Path, Rect } from "react-native-svg";
+import React, { useState } from "react";
+import Connexion from "../components/Connexion";
+import Inscription from "../components/Inscription";
 
 const LandingPage = () => {
 
   const navigate = useNavigate(); // Obtenir la fonction de navigation
 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showConnexion, setShowConnexion] = useState(false);
+  const [showInscription, setShowInscription] = useState(false);
+
+  const openConnexion = () => {
+    setShowConnexion(true);
+  };
+  const closeConnexion = () => {
+    setShowConnexion(false);
+  };
+  
+  const openInscription = () => {
+    setShowInscription(true);
+  };
+  const closeInscription = () => {
+    setShowInscription(false);
+  };
+
 
   return (
     <View>
       <Text style={styles.title}>Mystery Quest</Text>
-      <TouchableOpacity style={styles.button} onPress={() => {navigate("/HomePage")}}>
-        <Text>Jouer</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => {navigate("/Connexion")}}>
-        <Text>Connexion</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => {navigate("/Inscription")}}>
-        <Text>Inscription</Text>
-      </TouchableOpacity>
+      {isLoggedIn ? (
+        <TouchableOpacity style={styles.button} onPress={() => {navigate("/HomePage")}}>
+          <Text>Jouer</Text>
+        </TouchableOpacity>
+      ) : (
+        <><TouchableOpacity style={styles.button} onPress={openConnexion}>
+            <Text>Connexion</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={openInscription}>
+              <Text style={styles.desc} >Créer un compte</Text>
+          </TouchableOpacity>
+        </>
+      )
+    }
+    
+      {showConnexion && (
+        <View style={styles.overlay}>
+          <Connexion onClose={closeConnexion}/>
+        </View>
+      )}
+      {showInscription && (
+        <View style={styles.overlay}>
+          <Inscription onClose={closeInscription}/>
+        </View>
+      )}
     </View>
   );
 };
@@ -40,6 +77,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#EAB308',
     fontFamily: "Mystery",
 
+  },
+  desc: {
+    fontFamily: "Baskerville",
+    textAlign: "center",
+    lineHeight: 24,
+    fontSize: 12,
+    color: "#EAB308",
+    textDecorationLine:"underline"
+  },
+  overlay: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    // backgroundColor: "rgba(0, 0, 0, 0.3)",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
