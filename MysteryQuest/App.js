56774b4c79558ea9c9ac59908c7d8366f7173148
Alan <alan0613@live.fr>
2023-09-05 12:25:39 +0200

    new profil testing

--- MysteryQuest/App.js
+++ MysteryQuest/App.js
@@ -24,6 +24,8 @@ import RulesPage from "./pages/RulesPage";
 import TrophyPage from "./pages/TrophyPage";
 import Menu from "./components/Menu";
 import SettingPage from "./pages/SettingsPage";
+import EditUsername from "./pages/EditUsername";
+import EditPassword from "./pages/EditPassword";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -40,25 +42,24 @@ export default function App() {
         colors={["#0c0a09", "#4338ca"]}
         style={{ height: "100%", justifyContent: "center" }}
       >
-          <Routes>
-            <Route path="/home" element={<Home />} />
-            <Route path="/test" element={<Test />} />
-            <Route path="/HomePage" element={<HomePage />} />
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/ProfilePage" element={<ProfilePage />} />
-            <Route
-              path="/QuestRewardingPage"
-              element={<QuestRewardingPage />}
-            />
-            <Route path="/QuestStartPage" element={<QuestStartPage />} />
-            <Route path="/QuestStepPage" element={<QuestStepPage />} />
-            <Route path="/QuestSuccessPage" element={<QuestSuccessPage />} />
-            <Route path="/RankingPage" element={<RankingPage />} />
-            <Route path="/RulesPage" element={<RulesPage />} />
-            <Route path="/TrophyPage" element={<TrophyPage />} />
-            <Route path="/Menu" element={<Menu />} />
-            <Route path="/SettingPage" element={<SettingPage />} />
-          </Routes>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/test" element={<Test />} />
+          <Route path="/HomePage" element={<HomePage />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/ProfilePage" element={<ProfilePage />} />
+          <Route path="/QuestRewardingPage" element={<QuestRewardingPage />} />
+          <Route path="/QuestStartPage" element={<QuestStartPage />} />
+          <Route path="/QuestStepPage" element={<QuestStepPage />} />
+          <Route path="/QuestSuccessPage" element={<QuestSuccessPage />} />
+          <Route path="/RankingPage" element={<RankingPage />} />
+          <Route path="/RulesPage" element={<RulesPage />} />
+          <Route path="/TrophyPage" element={<TrophyPage />} />
+          <Route path="/Menu" element={<Menu />} />
+          <Route path="/SettingPage" element={<SettingPage />} />
+          <Route path="/EditUsername" element={<EditUsername />} />
+          <Route path="/EditPassword" element={<EditPassword />} />
+        </Routes>
       </LinearGradient>
     </NativeRouter>
   );
