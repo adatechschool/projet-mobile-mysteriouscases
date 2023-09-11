fb8241458d7ee4c54102274587e93f68f0b51f74
Camille Hebert <camille.hebert22@gmail.com>
2023-09-11 16:04:05 +0200

    added comments

--- back/app.js
+++ back/app.js
@@ -1,28 +1,30 @@
+//Création du serveur backend
 const express = require("express");
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Coucou je suis le backend");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`le back tourne sur le serveur ${port}`);
 });
 
+//Connexion à la Base de données
 const mysql = require('mysql2');
 
 require('dotenv').config(); // Charge les variables d'environnement depuis .env
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST,  //DB_HOST, DB_USER, etc = ce qui a été mis dans .env
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   port: process.env.DB_PORT,
 });
 
-
+//Vérification de la connexion
 connection.connect((err) => {
   if (err) {
     console.error('Erreur de connexion à la base de données :', err);
