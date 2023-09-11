20eeeed76b97efed3aae5b904150bf178ff5becb
Camille Hebert <camille.hebert22@gmail.com>
2023-09-11 15:55:35 +0200

    connexion db

--- back/app.js
+++ back/app.js
@@ -9,3 +9,24 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+const mysql = require('mysql2');
+
+require('dotenv').config(); // Charge les variables d'environnement depuis .env
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+});
+
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Erreur de connexion à la base de données :', err);
+    throw err;
+  }
+  console.log('Connecté à la base de données MySQL. Trop forte Camille !');
+});
