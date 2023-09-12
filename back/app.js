529c49df7d9a6c4173c5833b0054eb1818ebd6e0
Alan <alan0613@live.fr>
2023-09-12 11:27:42 +0200

    first attempt get route

--- back/app.js
+++ back/app.js
@@ -1,34 +1,32 @@
 //Création du serveur backend
 const express = require("express");
+const connectDB = require("./routes/db-config/db");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
+const connection = require("./routes/db-config/db");
+app.use(bodyParser.json());
+
+connectDB;
 
 app.get("/", (req, res) => {
   res.send("Coucou je suis le backend");
+});
+
+app.get("/users", (req, res) => {
+  connection.query("SELECT * FROM users", (error, data) => {
+    if (error) {
+      return;
+    } else {
+      res.send(data);
+    }
+  });
 });
 
 app.listen(port, () => {
   console.log(`le back tourne sur le serveur ${port}`);
 });
 
-//Connexion à la Base de données
-const mysql = require('mysql2');
-
-require('dotenv').config(); // Charge les variables d'environnement depuis .env
-
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,  //DB_HOST, DB_USER, etc = ce qui a été mis dans .env
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
-});
-
-//Vérification de la connexion
-connection.connect((err) => {
-  if (err) {
-    console.error('Erreur de connexion à la base de données :', err);
-    throw err;
-  }
-  console.log('Connecté à la base de données MySQL. Trop forte Camille !');
+app.post("/user", (req, res) => {
+  res.s;
 });
---
+++ back/routes/db-config/db.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql2");
+require("dotenv").config(); // Charge les variables d'environnement depuis .env
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST, //DB_HOST, DB_USER, etc = ce qui a été mis dans .env
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+});
+//Vérification de la connexion
+const connectDB = () => {
+  connection.connect((err) => {
+    if (err) {
+      console.error("Erreur de connexion à la base de données :", err);
+      throw err;
+    }
+    console.log("Connecté à la base de données MySQL. Trop forte Camille !");
+  });
+};
+module.exports = connectDB();
+module.exports = connection;
