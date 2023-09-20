a661f77e4fe0592806b48b3f7bc7390cff6d51b3
Sayadyna <sayadyna.m@gmail.com>
2023-09-20 16:00:17 +0200

    add start and finish time for the end of the quest

--- back/controllers/scoresController.js
+++ back/controllers/scoresController.js
@@ -1,4 +1,4 @@
-const connectDB = require("../routes/db-config/db")
+const connectDB = require("../routes/db-config/db");
 
 //Afficher tous les scores
 exports.getAllScores = (req, res) => {
@@ -14,8 +14,10 @@ exports.getAllScores = (req, res) => {
 
 //Afficher tous les scores d'une quête particulière
 exports.getQuestScores = (req, res) => {
-  const sql = "SELECT * FROM scores WHERE quest = ? ORDER BY CAST(score AS TIME) ASC"; // Ou "ORDER BY score ASC" si score n'est pas converti en time
-  connectDB.query(sql, (err, results) => {
+  const questId = req.params.questId;
+  const sql =
+    "SELECT * FROM scores WHERE quest = ? ORDER BY CAST(score AS TIME) ASC"; // Ou "ORDER BY score ASC" si score n'est pas converti en time
+  connectDB.query(sql, [questId], (err, results) => {
     if (err) {
       res.status(500).json({ error: err.message });
     } else {
@@ -24,18 +26,22 @@ exports.getQuestScores = (req, res) => {
   });
 };
 
-
 //Créer un nouveau score
 exports.postScore = (req, res) => {
-  const user = req.body.user
-  const quest = req.body.quest
-  const score = req.body.score
-  const date = req.body.date
-    if (!user || !quest || !score || !date) {
-      return res.status(400).json({ error: "Toutes les valeurs sont requises" });
-    }
-  const sql = "INSERT INTO scores (user, quest, score, date) VALUES (?,?,?,?)";
-  connectDB.query(sql, [user, quest, score, date], (error) => {
+  const user = req.body.user;
+  const quest = req.body.quest;
+  const start = req.body.start;
+  const finish = req.body.finish;
+  if (!user || !quest || !start || !finish) {
+    return res.status(400).json({ error: "Toutes les valeurs sont requises" });
+  }
+
+  // Calcul de la durée du score en secondes
+  const durationInSeconds = (finish - start) / 1000;
+
+  const sql =
+    "INSERT INTO scores (user, quest, start, finish) VALUES (?,?,?,?,?)";
+  connectDB.query(sql, [user, quest, durationInSeconds, finish], (error) => {
     if (error) {
       console.error(error);
       res.status(500).send({ error: err.message });
