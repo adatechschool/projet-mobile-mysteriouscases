dd3d043a0437f9da2318cb4f586e20e76f5dcc88
Camille Hebert <camille.hebert22@gmail.com>
2023-09-21 15:21:33 +0200

    changed db

--- back/db/mysteryquest_quests.sql
+++ back/db/mysteryquest_quests.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: mysteryquest
 -- ------------------------------------------------------
--- Server version	8.0.34
+-- Server version	8.1.0
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -50,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-09-19 16:02:40
+-- Dump completed on 2023-09-21 15:19:28
